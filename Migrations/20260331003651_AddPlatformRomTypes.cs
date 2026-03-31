97052300e9520464d0470b134bac1620948e779f
Josh Holsclaw <jholsclaw79@gmail.com>
2026-03-30 20:03:02 -0500

    GV-201: Added missing migration

---
+++ Migrations/20260331003651_AddPlatformRomTypes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameVault.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlatformRomTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RomTypes",
+                table: "Platforms",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RomTypes",
+                table: "Platforms");
+        }
+    }
+}
